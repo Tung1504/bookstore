9f63f9cb66a05356b85a0949f0a556ea0ebc068e
vietensight <viet.ensight@gmail.com>
2021-10-27 21:49:05 +0700

    update Personal Info Management && Validations && client-side UI

---
+++ bookstore/Controllers/PaymentCardController.cs
@@ -0,0 +1,86 @@
+﻿using bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookstore.Controllers
+{
+    // similar to AddressController
+    public class PaymentCardController : BaseController 
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            int user_id = Convert.ToInt32(Session["Id"]);
+
+            List<Payment_card> listPaymentCard = db.Payment_card.Where(a => a.user_id == user_id).ToList();
+
+            return View(listPaymentCard);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Payment_card paymentCard)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int user_id = Convert.ToInt32(Session["Id"]);
+                paymentCard.user_id = user_id;
+                db.Payment_card.Add(paymentCard);
+                db.SaveChanges(); //Apply insert statement
+                return RedirectToAction("Index");
+            }
+
+            //nếu validate thất bại
+            return View(paymentCard);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Payment_card paymentCard = db.Payment_card.Find(id);
+            if (paymentCard == null)
+            {
+                return HttpNotFound();
+            }
+            return View(paymentCard);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Payment_card paymentCard)
+        {
+            if (ModelState.IsValid)
+            {
+                int user_id = Convert.ToInt32(Session["Id"]);
+                paymentCard.user_id = user_id;
+                db.Entry(paymentCard).State = System.Data.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(paymentCard);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Payment_card paymentCard = db.Payment_card.Find(id);
+            if (paymentCard != null)
+            {
+                db.Payment_card.Remove(paymentCard);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return HttpNotFound();
+        }
+    }
+}
---
+++ bookstore/ViewModels/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+//not used (only used with UserInformation.cshtml when updated)
+namespace bookstore.ViewModels
+{
+    public class UserViewModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        // only for UpdateInformation (not require password anymore)
+        [Required(ErrorMessage = "'username' field is required")]
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string repassword { get; set; }
+
+
+        public string role { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public Nullable<System.DateTime> dob { get; set; }
+
+        public virtual ICollection<Address> Addresses { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+        public virtual ICollection<Payment_card> Payment_card { get; set; }
+
+    }
+}
